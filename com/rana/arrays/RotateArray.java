4eec2d17583c347c2c1736f84a1147116424005d
Rana Swami <ranaswam@gmail.com>
2022-01-09 23:40:25 +0530

    new files added

--- com/rana/arrays/RotateArray.java
+++ com/rana/arrays/RotateArray.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public class RotateArray {
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5};
+        int[] nums = {1,2,3,4,5,6,7};
 
-        System.out.println(Arrays.toString(moveByOne(nums)));
+        System.out.println(Arrays.toString(moveByOne(nums,3)));
     }
     static void rotateArray(int[] nums, int k) {
         int[] res = new int[nums.length];
@@ -20,13 +20,15 @@ public class RotateArray {
             res[i] = nums[j];
         }
     }
-    static int[] moveByOne(int[] nums){
-        
-        int temp;
+    static int[] moveByOne(int[] nums,int k){
 
-        for (int i = 1; i < nums.length; i++) {
-            temp = nums[i];
-            nums[i] = nums[i - 1];
+        for (int i = 1; i <= k; i++) {
+            int temp = nums[nums.length - 1];
+
+            for (int index = nums.length - 2; index >=0 ; index--) {
+                nums[index + 1] = nums[index];
+            }
+            nums[0] = temp;
         }
         return nums;
     }
