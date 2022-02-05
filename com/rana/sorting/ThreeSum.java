4abf47224079c82d713f6d755511bcafe4f1be93
Rana Swami <ranaswam@gmail.com>
2022-02-05 22:23:40 +0530

    new files added

--- com/rana/sorting/ThreeSum.java
+++ com/rana/sorting/ThreeSum.java
@@ -6,14 +6,14 @@ public class ThreeSum {
     public static void main(String[] args) {
         int[] nums = {-1,0,1,2,-1,-4};
 
-        List<List<Integer>> res = threeSum(nums);
+        List<List<Integer>> res = threeSum1(nums);
 
 
     }
     static List<List<Integer>> threeSum0(int[] nums) {
 
         HashSet<Integer> set = new HashSet<>();
-
+        int temp1, temp2, temp3;
         for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j < nums.length; j++) {
                 for (int k = j+1; k < nums.length; k++) {
@@ -21,6 +21,12 @@ public class ThreeSum {
                         set.add(i);
                         set.add(j);
                         set.add(k);
+                        temp1 = nums[i];
+                        temp2 = nums[j];
+                        temp3 = nums[k];
+                        nums[i] = Integer.MIN_VALUE;
+                        nums[j] = Integer.MIN_VALUE;
+                        nums[k] = Integer.MIN_VALUE;
                     }
                 }
             }
@@ -30,10 +36,68 @@ public class ThreeSum {
         return list;
     }
     static List<List<Integer>> threeSum(int[] nums) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int count = 1;
+            if (hm.containsKey(nums[i])){
+                int count0 = hm.get(nums[i]);
+                hm.put(nums[i], count0++);
+            }else if (!hm.containsKey(nums[i])){
+                hm.put(nums[i], count);
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int count0 = hm.get(nums[i]); // ensure count will be one of 0,1,2,3,...
+            hm.put(nums[i], count0--);
+            for (int j = i+1; j < nums.length; j++) {
+                int count = hm.get(nums[j]); // ensure count will be one of 0,1,2,3,...
+                hm.put(nums[j], count--);
+
+                int c = -(nums[i]+nums[j]);
+                if (hm.containsKey(c)){
+
+                }
+                hm.put(nums[j], count++);
+            }
+            hm.put(nums[i], count0++);
+        }
 
         List<List<Integer>> list = new ArrayList<>();
 
         return list;
+    }
+    static List<List<Integer>> threeSum1(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        if(nums.length == 0)
+            return res;
+        
+        int[] arr = mergeSort(nums);
+
+        for (int i = 0; i < arr.length - 2; i++) {//because our a can go to 3rd last element in the array
+
+            if (i == 0 ||(i > 0 && arr[i] != arr[i-1])) { //to skip the repeating elements like -1,-1,-1,-1 which are in the starting of the sorted array
+
+                int low = i+1, high = arr.length-1, sum = 0 - arr[i];
+
+                while (low < high){
+                    if (arr[low] + arr[high] == sum){
+                        res.add(Arrays.asList(arr[i], arr[low], arr[high]));
+
+                        while (low < high && arr[low] == arr[low+1]) low++;
+                        while (low < high && arr[high] == arr[high-1]) high--;
+                        low++;
+                        high--;
+                    }
+                    else if (arr[low] + arr[high] < sum) low++;
+                    else high--;
+                }
+            }
+        }
+
+        return res;
     }
     static int[] mergeSort(int[] nums){
         if (nums.length == 1)
