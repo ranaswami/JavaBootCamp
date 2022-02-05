6764ec655348deb1b5ae12b31702e6c1ee24b2bc
Rana Swami <ranaswam@gmail.com>
2022-02-05 23:50:42 +0530

    new files added

--- com/rana/sorting/MaximumProduct.java
+++ com/rana/sorting/MaximumProduct.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 public class MaximumProduct {
     public static void main(String[] args) {
-
+        int[] nums = {1,2,3};
+        System.out.println(maximumProduct(nums));
     }
     static int maximumProduct(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
