8c1bdfcb5711dcabafe9a2f7a8c28063c8d3b353
Rana Swami <ranaswam@gmail.com>
2022-03-09 14:34:30 +0530

    new files added

---
+++ com/rana/dynamicProgramming/CoinChange.java
@@ -0,0 +1,40 @@
+package com.rana.dynamicProgramming;
+//https://leetcode.com/problems/coin-change/
+public class CoinChange {
+    public static void main(String[] args) {
+        int[] coins = {1,2,5};
+        int sum = 11;
+
+        System.out.println(coinChange(coins, sum));
+    }
+    static int coinChange(int[] coins, int amount) {
+        int[][] t = new int[coins.length+1][amount+1];
+
+        for (int i = 0; i < t[0].length; i++) {//1st row
+            t[0][i] = Integer.MAX_VALUE - 1;
+        }
+
+        for (int i = 1; i < t[0].length; i++) {//2nd row
+            t[1][i] = Integer.MAX_VALUE - 1;
+        }
+
+        for (int i = 1; i < t.length; i++) {//1st column
+            t[i][0] = 0;
+        }
+
+        for (int i = 1; i < t.length; i++) {
+            for (int j = 1; j < t[0].length; j++) {
+                if (coins[i-1] <= j){
+                    t[i][j] = Math.min(t[i-1][j], t[i][j - coins[i-1]] + 1);
+                }
+                else
+                    t[i][j] = t[i-1][j];
+            }
+        }
+
+        if (t[coins.length][amount] >= Integer.MAX_VALUE - 1)
+            return -1;
+        else
+            return t[coins.length][amount];
+    }
+}
