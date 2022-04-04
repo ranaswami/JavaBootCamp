62feb8a1e86cff02ace6b15a4f12de20dd4bbbe0
Rana Swami <ranaswam@gmail.com>
2022-04-04 22:41:24 +0530

    new files added

---
+++ com/rana/bitwise/SubsetXORSum.java
@@ -0,0 +1,10 @@
+package com.rana.bitwise;
+
+public class SubsetXORSum {
+    public static void main(String[] args) {
+        int[] nums = {3,4,5,6,7,8};
+    }
+    static int subsetXORSum(int[] nums) {
+        return 0;
+    }
+}
