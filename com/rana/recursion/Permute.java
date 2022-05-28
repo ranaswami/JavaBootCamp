68e173f9c716e49e251ecdb5fdb95e797287b762
Rana Swami <ranaswam@gmail.com>
2022-05-28 23:37:46 +0530

    new files modified

--- com/rana/recursion/Permute.java
+++ com/rana/recursion/Permute.java
@@ -7,10 +7,10 @@ import java.util.List;
 public class Permute {
     public static void main(String[] args) {
         int[] nums = {1,2,3};
+        System.out.println(Arrays.toString(permute1(nums).toArray()));
         System.out.println(Arrays.toString(permute(nums).toArray()));
-
     }
-    static List<List<Integer>> permute(int[] nums) {
+    static List<List<Integer>> permute1(int[] nums) {
         if (nums ==  null || nums.length == 0){
             return new ArrayList<>();
         }
@@ -40,5 +40,31 @@ public class Permute {
             used[i] = false;
             currList.remove(currList.size()-1);
         }
+    }
+    static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        recurPermute(0, nums, res);
+        return res;
+    }
+    static void recurPermute(int index, int[] nums, List<List<Integer>> res){
+        if (index == nums.length){
+            //copy the output to result
+            List<Integer> op = new ArrayList<>();
+            for (int num : nums) {
+                op.add(num);
+            }
+            res.add(new ArrayList<>(op));
+            return;
+        }
+        for (int i = index; i < nums.length; i++) {
+            swap(i, index, nums);
+            recurPermute(index+1, nums, res);
+            swap(i, index, nums);
+        }
+    }
+    static void swap(int i, int j, int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
